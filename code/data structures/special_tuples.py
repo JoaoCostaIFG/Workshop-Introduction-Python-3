9ca1caf43c5154b6de103f7c4c06cc1fc74b18e1
JoaoCostaIFG <joaocosta.tra@outlook.com>
2019-09-21 19:22:54 +0100

    Code for the Tuples part in the Data Structures Section is ready and was added

---
+++ code/data structures/special_tuples.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Create an empty tuple
+emptytuple = ()
+
+# Create a tuple with only 1 element
+singletuple = (1, )  # Notice the trailing comma
+
+print("Our tuples:", emptytuple, singletuple)
+
+print("emptytuple's len:", len(emptytuple))
+print("singletuple:", len(singletuple))
+
