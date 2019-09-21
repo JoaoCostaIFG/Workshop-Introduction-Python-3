62876d4a505abc49012d445aeabb686948c65153
JoaoCostaIFG <joaocosta.tra@outlook.com>
2019-09-21 20:30:42 +0100

    sets code is not ready yet

---
+++ code/data structures/sets.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Create a set
+aset = {"yum", "sweet", "potato", "yum"}
+# Equivalent to aset = set(['yum', 'sweet', 'potato'])
+print("Our set:", aset)
+
+# Check membership
+if "yum" in aset:
+    print(True)
+
+else:
+    print(False)
+
+if "Iargo" in aset:
+    print(True)
+
+else:
+    print(False)
+
+
