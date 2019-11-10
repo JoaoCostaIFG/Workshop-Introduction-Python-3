f513d689ec2a3dc45b1f22c3b1ffca505e333176
Tiago Silva <tiagodusilva@gmail.com>
2019-11-10 22:04:17 +0000

    Added comments to solutions

--- solutions/5.py
+++ solutions/5.py
@@ -1,11 +1,12 @@
+#  Function with 2 arguments, the full string and the separator
 def convert_camel_case(string, sep):
     res = ""
     for char in string:
-        if char.upper() == char:
+        if char.upper() == char:  # If it's an uppercase, add it lowered with the sep
             res += sep + char.lower()
         else:
-            res += char
+            res += char  # Else add the same char
     return res
 
 string = input()
-print(convert_camel_case(string, "_"))
+print(convert_camel_case(string, "_"))  # Call the function and print its return value
