c2793f91612bd692f8e50d88ca96f217a316c537
tiago <tiagodusilva@gmail.com>
2019-11-12 10:52:19 +0000

    Plz work

--- solutions/5.py
+++ solutions/5.py
@@ -2,11 +2,13 @@
 def convert_camel_case(string, sep):
     res = ""
     for char in string:
-        if char.upper() == char:  # If it's an uppercase, add it lowered with the sep
+        if char.isupper():  # If it's an uppercase, add it lowered with the sep
             res += sep + char.lower()
         else:
             res += char  # Else add the same char
     return res
 
 string = input()
-print(convert_camel_case(string, "_"))  # Call the function and print its return value
+sep = input()
+print("Function:", convert_camel_case(string, sep))  # Call the function and print its return value
+print("Generator:", "".join([(sep + letter.lower()) if letter.isupper() else letter for letter in string]))
